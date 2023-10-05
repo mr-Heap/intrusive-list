1adb5b664ccb9b009af70b538819dd11289ac32e
Marina Troshchiy <marina_troshchiy@mail.ru>
2023-10-06 01:11:57 +0300

    Refactor code

--- intrusive_list.cpp
+++ intrusive_list.cpp
@@ -1,10 +1,3 @@
 #include "intrusive_list.h"
 
-//void intrusive::list_base::unlink(intrusive::list_base * node) {
-//  node->prev = nullptr;
-//  node->next = nullptr;
-//}
-
-
-
 
--- intrusive_list.h
+++ intrusive_list.h
@@ -1,28 +1,21 @@
 #pragma once
 
-#include <iostream> // delete later
+#include <iostream>
 
 namespace intrusive {
 struct default_tag;
 
 struct list_base {
 
-  list_base * prev;
-  list_base * next;
-
-//  void unlink(list_base * node);
+  list_base* prev;
+  list_base* next;
 
   list_base() = default;
 
-  list_base(list_base * prev, list_base * next)
-      : prev(prev)
-      , next(next) {
-  }
+  list_base(intrusive::list_base* prev, intrusive::list_base* next)
+      : prev(prev), next(next) {}
 
-  ~list_base() {
-//    unlink(prev);
-//    unlink(next);
-  }
+  ~list_base() = default;
 };
 
 template <typename Tag = default_tag>
@@ -35,88 +28,70 @@ struct list {
 
 public:
   template <bool is_const>
-  class my_iterator
-  {
+  class my_iterator {
   public:
+    list_base* it;
 
-    list_base * it;
-
-    template<bool WasConst, class = std::enable_if_t<is_const || !WasConst>>
+    template <bool WasConst, class = std::enable_if_t<is_const || !WasConst>>
     my_iterator(my_iterator<WasConst> rhs) : it(rhs.it) {}
     using iterator_category = std::bidirectional_iterator_tag;
     using difference_type = std::ptrdiff_t;
     using value_type = list_element<Tag>;
-//    using pointer = T*;
-//    using reference = T&;
     using pointer = typename std::conditional<is_const, const T*, T*>::type;
     using reference = typename std::conditional<is_const, const T&, T&>::type;
 
     my_iterator() = default;
 
-    my_iterator(list_base * it)
-        : it(it)
-    {
+    explicit my_iterator(list_base* it) : it(it) {}
+
+    my_iterator& operator=(my_iterator& other) {
+      this->it = other.it;
     }
 
-//    my_iterator& operator=(my_iterator & other) {                             // TODO
-//      this->it = other.it;
-//    }
+    my_iterator& operator=(const my_iterator other) {
+      this->it = other.it;
+    }
 
-//    my_iterator& operator=(const my_iterator other) {
-//      this->it = other.it;
-//    }
-
-    friend bool operator==(const my_iterator & lhs, const my_iterator & rhs)
-    {
+    friend bool operator==(const my_iterator& lhs, const my_iterator& rhs) {
       return lhs.it == rhs.it;
     }
 
-    friend bool operator!=(const my_iterator & lhs, const my_iterator & rhs)
-    {
+    friend bool operator!=(const my_iterator& lhs, const my_iterator& rhs) {
       return lhs.it != rhs.it;
     }
 
-    reference operator*() const
-    {
+    reference operator*() const {
       return static_cast<reference>(*it);
     }
 
-    pointer operator->() const
-    {
+    pointer operator->() const {
       return static_cast<pointer>(static_cast<list_element<Tag>*>(it));
     }
 
-
-    my_iterator & operator++()
-    {
+    my_iterator& operator++() {
       it = it->next;
       return *this;
     }
 
-    my_iterator operator++(int)
-    {
+    my_iterator operator++(int) {
       auto tmp = *this;
       operator++();
       return tmp;
     }
 
-    my_iterator & operator--()
-    {
+    my_iterator& operator--() {
       it = it->prev;
       return *this;
     }
 
-    my_iterator operator--(int)
-    {
+    my_iterator operator--(int) {
       auto tmp = *this;
       operator--();
       return tmp;
     }
-
   };
   using iterator = my_iterator<false>;
   using const_iterator = my_iterator<true>;
-
 
   iterator begin() const {
     return iterator(sentinel.next);
@@ -126,7 +101,8 @@ public:
     return iterator(sentinel.next->prev);
   }
 
-  iterator insert (iterator next, T & element) {                                // you can't insert an element twice
+  iterator insert(iterator next,
+                  T& element) { // you can't insert an element twice
     for (auto i = this->begin(); i != this->end(); ++i) {
       if (&(*i) == &element) {
         if (i == next) {
@@ -136,7 +112,7 @@ public:
         --i;
       }
     }
-    iterator a = iterator(static_cast<list_element<Tag> *>(&element));
+    iterator a = iterator(static_cast<list_element<Tag>*>(&element));
     a.it->next = next.it;
     a.it->prev = (--next).it;
     next.it->next = a.it;
@@ -146,7 +122,7 @@ public:
     return a;
   }
 
-  iterator erase (iterator position) {
+  iterator erase(iterator position) {
     --position;
     position.it->next = position.it->next->next;
     position.it->next->prev = position.it;
@@ -158,8 +134,7 @@ public:
     sentinel.next = &sentinel;
   }
 
-  list(list && other)
-  {
+  list(list&& other) {
     if (other.empty()) {
       this->sentinel.prev = &sentinel;
       this->sentinel.next = &sentinel;
@@ -176,7 +151,7 @@ public:
     other.sentinel.next = &other.sentinel;
   }
 
-  list& operator=(list && other) {
+  list& operator=(list&& other) {
     if (other.empty()) {
       this->sentinel.prev = &sentinel;
       this->sentinel.next = &sentinel;
@@ -194,10 +169,11 @@ public:
     return *this;
   }
 
-  void splice(const_iterator start, list<T> & from,
-              const_iterator position_from,
+  void splice(const_iterator start, list<T>& from, const_iterator position_from,
               const_iterator position_to) {
-    if (position_to == position_from) return;
+    if (position_to == position_from) {
+      return;
+    }
 
     --position_to;
     position_from.it->prev->next = position_to.it->next;
@@ -207,10 +183,9 @@ public:
     position_to.it->next = start.it;
     position_from.it->prev->next = position_from.it;
     position_to.it->next->prev = position_to.it;
-
   }
 
-  void push_back(T& element)  {
+  void push_back(T& element) {
     auto* tmp = static_cast<list_element<Tag>*>(&element);
     if (tmp->prev == tmp->next) {
       tmp->prev->prev = tmp->prev;
@@ -231,55 +206,36 @@ public:
   }
 
   T& front() {
-    if (empty()) {
-      throw 123;
-    }
-    return *static_cast<T *>(static_cast<list_element<Tag> *>(sentinel.next));
+    return *static_cast<T*>(static_cast<list_element<Tag>*>(sentinel.next));
   }
 
   const T& front() const {
-    if (empty()) {
-      throw 123;
-    }
-    return *static_cast<T *>(static_cast<list_element<Tag> *>(sentinel.next));
+    return *static_cast<T*>(static_cast<list_element<Tag>*>(sentinel.next));
   }
 
   T& back() {
-    if (empty()) {
-      throw 123;
-    }
-    return *static_cast<T *>(static_cast<list_element<Tag> *>(sentinel.prev));
+    return *static_cast<T*>(static_cast<list_element<Tag>*>(sentinel.prev));
   }
 
   const T& back() const {
-    if (empty()) {
-      throw 123;
-    }
-    return *static_cast<T *>(static_cast<list_element<Tag> *>(sentinel.prev));
+    return *static_cast<T*>(static_cast<list_element<Tag>*>(sentinel.prev));
   }
 
   void pop_front() {
-
     sentinel.next->next->prev = &sentinel;
     sentinel.next = sentinel.next->next;
-
   }
 
   void pop_back() {
-
     sentinel.prev->prev->next = &sentinel;
     sentinel.prev = sentinel.prev->prev;
-
   }
 
   bool empty() const {
     return sentinel.next == &sentinel && sentinel.prev == &sentinel;
   }
 
-  // destructor - ?
-
   list_element<Tag> sentinel;
-
 };
 
 } // namespace intrusive
